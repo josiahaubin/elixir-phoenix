f92a82623329fc38bc2aab1719b1ec5bf257650f
josiahaubin <josiahaubin@gmail.com>
2022-03-03 14:09:39 -0700

    adding more code

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -2,6 +2,16 @@ defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    # |> put_flash(:info, "welcome back!")
+    # |> put_resp_content_type("text/plain")
+    |> assign(:var, "I am a string")
+    |> render("index.html")
+
+    # render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
   end
 end
